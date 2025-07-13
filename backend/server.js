df2caed582ef5525b2a6d0202b28b1f516349280
Hosanna-Mosa <Hosannamosa4190@gmail.com>
2025-07-13 21:57:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "*"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://beaten1-1.onrender.com", "*"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
